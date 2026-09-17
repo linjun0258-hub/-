2a56c5eadc0254f8ac1bb2e761bb867462ff7484
linjun0258-hub <linjun0258@gmail.com>
2026-09-18 07:58:26 +0800

    Add API handler using Fresh framework

---
+++ 17fei-main/api/index.ts
@@ -0,0 +1,11 @@
+import { createHandler } from "$fresh/server.ts";
+import manifest from "../fresh.gen.ts";
+import config from "../fresh.config.ts";
+
+if (!globalThis.__freshHandler) {
+  globalThis.__freshHandler = await createHandler(manifest, config);
+}
+
+export default async function handler(req: Request) {
+  return await globalThis.__freshHandler(req);
+}
