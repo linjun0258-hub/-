d774d21734a9ba93648951691752a138392fbebe
linjun0258-hub <linjun0258@gmail.com>
2026-09-25 16:05:17 +0800

    Use esm.sh bcryptjs for password hashing

--- 17fei-main/lib/userAuth.ts
+++ 17fei-main/lib/userAuth.ts
@@ -1,5 +1,5 @@
 // 用户认证模块：bcrypt 密码哈希 + 签名 Cookie 会话
-import { hash, compare } from "https://esm.sh/bcrypt@5.1.1";
+import { hash, compare } from "https://esm.sh/bcryptjs@2.4.3";
 import { createHmac, timingSafeEqual } from "node:crypto";
 
 const SESSION_SECRET = Deno.env.get("SESSION_SECRET") ?? "dev-secret-change-me";
